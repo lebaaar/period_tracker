4ff7cd6b822b7d4b8109377684f0ff974349df68
Lan <lanlebar5@gmail.com>
2025-07-06 23:25:39 +0200

    log period button

--- lib/log_period_page.dart
+++ lib/log_period_page.dart
@@ -15,7 +15,10 @@ class LogPeriodPage extends StatelessWidget {
       appBar: AppBar(
         backgroundColor: Colors.transparent,
         elevation: 0,
-        title: const Text('Log Period', style: TextStyle(color: Colors.white)),
+        title: const Text(
+          'Add Period',
+          style: TextStyle(color: Colors.white),
+        ), // or edit period if editing
         centerTitle: true,
         leading: IconButton(
           icon: const Icon(Icons.close),
@@ -24,22 +27,20 @@ class LogPeriodPage extends StatelessWidget {
         ),
       ),
       body: const Center(child: Text('Add or edit your period details here.')),
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.all(100.0),
-        child: SizedBox(
-          width: double.infinity,
-          height: 48,
-          child: ElevatedButton(
-            onPressed: () => _onSave(context),
-            style: ElevatedButton.styleFrom(
-              backgroundColor: Theme.of(context).colorScheme.primary,
-              foregroundColor: Theme.of(context).colorScheme.onPrimary,
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(12),
-              ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
+      floatingActionButton: Container(
+        height: 50,
+        margin: const EdgeInsets.all(10),
+        child: ElevatedButton(
+          onPressed: () => _onSave(context),
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Theme.of(context).colorScheme.primary,
+            foregroundColor: Theme.of(context).colorScheme.onPrimary,
+            shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(12),
             ),
-            child: const Text('Save'),
           ),
+          child: const Center(child: Text('Save')),
         ),
       ),
     );
